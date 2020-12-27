74e5c71017700d1aeb48c2d45e8cc34b0a501a73
KpuTuK <kputuk@debian8>
2020-12-27 22:50:42 +0300

    UPD Добавлены @phpdoc, добавлен метод getValidator() возвращающий обьект класса System\Engine\Validator с содержимым корзины

--- System/Engine/Http/ParameterBag.php
+++ System/Engine/Http/ParameterBag.php
@@ -9,13 +9,20 @@
 
 namespace System\Engine\Http;
 
+use System\Engine\Validator;
+
 /**
- * Description of ParametrBag
- *
+ * Корзина параметров
  * @author KpuTuK
  */
 class ParameterBag extends \ArrayObject
 {
+    /**
+     * Возвращает отфильтрованое значение согласно ключу
+     * @see https://www.php.net/manual/ru/arrayaccess.offsetget
+     * @param string $index ключ
+     * @return mixed значение по ключу
+     */
     public function offsetGet($index)
     {
         if (!$this->offsetExists($index)) {
@@ -23,8 +30,21 @@ class ParameterBag extends \ArrayObject
         }
         return htmlspecialchars(trim(parent::offsetGet($index)));
     }
+    /**
+     * Добавляет массив в корзину
+     * @param array $array Добавляемый массив
+     * @return array Массив корзины
+     */
     public function merge(array $array = [])
     {
         return $this->exchangeArray(array_merge($this->getArrayCopy(), $array));
+    }
+    /**
+     * Возвращает обьект валидатора с массивом данных корзины
+     * @return System\Engine\Validator
+     */
+    public function getValidator()
+    {
+        return new Validator($this->getArrayCopy());
     }
 }
